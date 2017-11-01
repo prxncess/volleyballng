b58c36b90eaf1dec5506f5d623c75ce04b8b6bd8
prxncess <pchikeogwo@gmail.com>
2017-11-01 21:50:02 +0100

    Edits + reasons
    I moved 'events description' because in its current position, it doesn't look good on mobile (except the demo I saw using Inspect Element is inaccurate - I'll check again when the site is back up)

--- resources/views/admin/events/index.blade.php
+++ resources/views/admin/events/index.blade.php
@@ -10,14 +10,14 @@
         <div class="well well" id="admin-box">
             <header>
                 <h3 class="text-center"><i class="fa fa-paperclip"></i> Event</h3>
-                <div id="separator"></div>
-                <a href="{{route('createEvent')}}" class="pull-right"><i class="fa fa-plus"></i> New event</a>
+                <div id="" class="purple-separator top-10 bottom-20"></div>
+                <a href="{{route('createEvent')}}" class="float-right btn btn-purple bottom-10"><i class="fa fa-plus"></i> New event</a>
             </header>
             @if(session('res'))
             <div class="alert alert-danger">{{session('res')}}</div>
             @endif
             @if($events->isEmpty())
-                <div class="alert alert-warning">There are currently no events yet</div>
+                <div class="alert alert-warning">No events yet. Watch this space, or create an event!</div>
                 @else
                 {{--show events--}}
             @foreach($events as $event)
@@ -28,16 +28,19 @@
                     </div>
                     <div class="media-body">
                         <h4 class="media-heading">{{$event->title}}</h4>
-                        <p class="">{{$event->description}}</p>
+                        
                     </div>
                     <div class="media-bottom">
+                        <p class="">{{$event->description}}</p>
                         <ul class="list-unstyled list-inline">
                             <li><i class=""></i><strong>Loaction:</strong> <span>{{$event->e_location}}</span> </li>
                             <li><i class=""></i><strong>Organizer:</strong> <span>{{$event->e_organizer}}</span> </li>
                             <li><i class=""></i><strong>Contact:</strong> <span>{{$event->e_email}}</span> </li>
-                            <li><i class="fa fa-eye"></i> <a href="{{route('showEvent',$event->slug)}}">More</a> </li>
-                            <li><i class="fa fa-edit"></i> <a href="{{route('editEvent',$event->slug)}}">Edit</a> </li>
                         </ul>
+                        <p>
+                            <a href="{{route('showEvent',$event->slug)}}" class="right-10"><i class="fa fa-ellipsis-h right-5"></i> More</a>
+                            <a href="{{route('editEvent',$event->slug)}}" class="right-10"><i class="fa fa-edit right-5"></i>Edit</a>
+                        </p>
 
                     </div>
                     <div id="separator"></div>
