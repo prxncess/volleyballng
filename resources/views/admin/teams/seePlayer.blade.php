b66cea3456b7f3f8110240119fc52aefb1729585
michael <kulblog66@gmail.com>
2017-11-19 22:11:46 +0100

    fixed player feet and inches to be displayed on admin view player

--- resources/views/admin/teams/seePlayer.blade.php
+++ resources/views/admin/teams/seePlayer.blade.php
@@ -20,7 +20,7 @@
                     <div class="col-sm-7">
                         <ul class="list-unstyled">
                             <li>Position: <span>{{$player->position}}</span></li>
-                            <li>Height: <span>{{$player->height}}</span></li>
+                            <li>Height: <span>{{$player->feet.' '.$player->inches}}</span></li>
                             <li>Age: <span>Not available</span></li>
                         </ul>
                         <a href="{{route('editPlayer',[$team->name,$player->id])}}" id="editPlayer" class="btn btn-warning"><i class="fa fa-edit"></i> Edit </a>
